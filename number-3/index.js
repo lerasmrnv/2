7cb0cb1e6b47c2d944ed6cc911dd2b0f90f5990b
lerasmrnv <lera.smirnova.flok@gmail.com>
2022-02-20 22:25:01 +0300

    Change js

--- number-3/index.js
+++ number-3/index.js
@@ -7,86 +7,36 @@ const userName = document.querySelector('.userName');
 const comment = document.querySelector('.comment');
 const img = document.querySelector('.img');
 
-const message = {
-    img: img,
-    name: userName,
-    comment: comment
+let messagesArray = [];
+// if (localStorage.getItem('messages')){
+//     messages = JSON.parse(localStorage.getItem('messages'));
+// }
+localStorage.setItem('messages', JSON.stringify(messagesArray));
+const data = JSON.parse(localStorage.getItem('messages'));
+
+function createP(){
+    const p = document.createElement('p');
+    p.innerHTML = `<img width="40px" hight="auto" src="${img.value}">  ${userName.value} : ${comment.value}`;
+    
+    div.appendChild(p);
 }
-
-localStorage.setItem("message", JSON.stringify(message));
-function readUser() {
-    const messageJSON = localStorage.getItem("message")
-
-    if (messageJSON === null) {
-    return undefined;
-    }
-}
-
 
 btn.addEventListener('click', function(evt){
     evt.preventDefault();
 
-    const p = document.createElement('p');
-    p.className = 'message';
-    p.innerHTML = `<img width="40px" hight="auto" src="${img.value}">  ${userName.value} : ${comment.value}`;
-    
-    div.appendChild(p);
+    messagesArray.push({
+        url: img.value,
+        name: userName.value,
+        text: comment.value
+    })
+    localStorage.setItem('messages', JSON.stringify(messagesArray));
 
+    createP();
     comment.value = '';
 })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// document.addEventListener("DOMContentLoaded", function(evt){
-//     let userName = localStorage.getItem('userName');
-//     let img = localStorage.getItem('img');
-//     if (userName != null && img != null){
-//         document.querySelector('.userName').value = userName;
-//         document.querySelector('.img').value = img;
-//     }
-// })
-
-// let btn = document.querySelector('.btn');
-
-
-// function sendMessage(img, userName, comment){
-//     document.querySelector("img").innerHTML = `src=""` 
-//     document.querySelector(".messages").innerHTML += `<span>${userName} : </span><span>${comment}</span></br>`; 
-// }
-
-// btn.addEventListener('click', function(evt){
-//     let userName = document.querySelector('.userName').value;
-// let comment = document.querySelector('.comment').value;
-// let img = document.querySelector('.img').value;
-
-//     if (localStorage.getItem ('.userName') === null) {
-//         localStorage.setItem('userName', userName);
-//     }
-//     if (localStorage.getItem('.img') === null){
-//         localStorage.setItem('img', img);
-//     }
-//     sendMessage(img, userName, comment);
-    
-// });
-
+data.forEach(messages => {
+    createP(messages);
+});
 
 
